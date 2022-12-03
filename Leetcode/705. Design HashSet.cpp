8a28d02b0a0acf0f17931c1ea7c02ff054a25b0c
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2022-12-03 22:02:45 +0530

    Create 705. Design HashSet.cpp

---
+++ Leetcode/705. Design HashSet.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+public:
+    vector<int>v;
+    
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        if(!contains(key))
+            v.push_back(key);
+    }
+    
+    void remove(int key) {
+        if(contains(key))
+            v.erase(find(v.begin(),v.end(),key));
+    }
+    
+    bool contains(int key) {
+        return find(v.begin(),v.end(),key)!=v.end();
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
