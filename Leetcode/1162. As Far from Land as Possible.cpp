a28ad118a67ce5c9cf07b8da5b3989b278890cd1
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-02-10 18:05:28 +0530

    Create 1162. As Far from Land as Possible.cpp

---
+++ Leetcode/1162. As Far from Land as Possible.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        const int MAX_DISTANCE = rows + cols + 1;
+        
+        vector<vector<int>> dist(rows, vector<int> (cols, MAX_DISTANCE));
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j]) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = min(dist[i][j], min(i > 0 ? dist[i - 1][j] + 1 : MAX_DISTANCE,
+                                                     j > 0 ? dist[i][j - 1] + 1 : MAX_DISTANCE));
+                }
+            }
+        }
+
+        int ans = INT_MIN;
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                dist[i][j] = min(dist[i][j], min(i < rows - 1 ? dist[i + 1][j] + 1 : MAX_DISTANCE,
+                                                 j < cols - 1 ? dist[i][j + 1] + 1 : MAX_DISTANCE));
+                
+                ans = max(ans, dist[i][j]);
+            }
+        }
+        
+        return ans == 0 || ans == MAX_DISTANCE ? -1 : ans;
+    }
+};
