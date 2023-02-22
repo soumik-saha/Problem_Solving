57d28f83dcaeffe1fe44873c331f875e2a3b06b8
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-02-22 10:44:35 +0530

    Update 198. House Robber.cpp

--- Leetcode/198. House Robber.cpp
+++ Leetcode/198. House Robber.cpp
@@ -1,25 +1,16 @@
 class Solution {
-private:
-    int n;
-    vector<int> cache;
-    const int INF = 987654321;
-    int rob(vector<int>& nums, int index) {
-        if (index == n) return 0;
-        if (index > n) return -INF;
-        
-        int &ret = cache[index];
-        if (ret != -1) return ret;
-        
-        int result = nums[index];
-        result = max(result, nums[index] + rob(nums, index + 2));
-        result = max(result, nums[index] + rob(nums, index + 3));
-        return ret = result;
-    }
-    
 public:
     int rob(vector<int>& nums) {
-        n = nums.size();
-        cache = vector<int>(n, -1);
-        return max(rob(nums, 0), rob(nums, 1));
+        int m1=0, m2=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(i%2==0){
+                m1 = max(m1+nums[i], m2);
+            }
+            else{
+                m2 = max(m1, m2+nums[i]);
+            }
+        }
+        return max(m1, m2);
     }
 };
