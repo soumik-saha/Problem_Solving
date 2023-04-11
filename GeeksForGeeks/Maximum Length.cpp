2467aff8ea4e127d0d6c6107ee5ea6b90860c014
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-04-11 18:10:34 +0530

    Create Maximum Length.cpp

---
+++ GeeksForGeeks/Maximum Length.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        int arr[3] = {a,b,c};
+        sort(arr, arr+3);
+        
+        if(arr[2]>2*(arr[0]+arr[1]+1)){
+            return -1;
+        }
+        
+        return a+b+c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
