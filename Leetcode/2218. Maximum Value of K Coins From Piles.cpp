685f0c96c61f85c5fc954af2279124474b4bca12
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-04-15 17:57:29 +0530

    Update 2218. Maximum Value of K Coins From Piles.cpp

--- Leetcode/2218. Maximum Value of K Coins From Piles.cpp
+++ Leetcode/2218. Maximum Value of K Coins From Piles.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n = piles.size();
-        function<int(int, int)> f = [&](int i, int coins) {
-            if (i == 0) {
-                return 0;
-            }
-            int result = 0, currentSum = 0;
-            for (int currentCoins = 0; currentCoins <= min((int)piles[i - 1].size(), coins); currentCoins++) {
-                if (currentCoins > 0) {
-                    currentSum += piles[i - 1][currentCoins - 1];
+        vector dp(n + 1, vector<int>(k + 1));
+        for (int i = 1; i <= n; i++) {
+            for (int coins = 0; coins <= k; coins++) {
+                int currentSum = 0;
+                for (int currentCoins = 0; currentCoins <= min((int)piles[i - 1].size(), coins); currentCoins++) {
+                    if (currentCoins > 0) {
+                        currentSum += piles[i - 1][currentCoins - 1];
+                    }
+                    dp[i][coins] = max(dp[i][coins], dp[i - 1][coins - currentCoins] + currentSum);
                 }
-                result = max(result, f(i - 1, coins - currentCoins) + currentSum);
             }
-            return result;
-        };
-        return f(n, k);
+        }
+        return dp[n][k];
     }
 };
