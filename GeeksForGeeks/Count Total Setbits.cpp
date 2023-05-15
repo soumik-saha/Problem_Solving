b81bf053bf84b038738131e9d4661925468da9b9
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-05-15 21:19:56 +0530

    Create Count Total Setbits.cpp

---
+++ GeeksForGeeks/Count Total Setbits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long countBits(long long N) {
+        long long cntSetBits = 0;
+        for(int i=0; i<N; i++) {
+            cntSetBits += __builtin_popcount(i+1);
+        }
+        return cntSetBits;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        long long N;
+        scanf("%lld",&N);
+        
+        Solution obj;
+        long long res = obj.countBits(N);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
