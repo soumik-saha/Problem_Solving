eb5e02cd01101a6a50f1de2a43734a061fb6e6bd
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-05-31 19:58:47 +0530

    Create 1396. Design Underground System.cpp

---
+++ Leetcode/1396. Design Underground System.cpp
@@ -0,0 +1,28 @@
+class UndergroundSystem {
+public:
+    vector<vector<int>> v;
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        for(int i=0; i)
+            v.push_back()
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
