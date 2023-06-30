24b34c3db29e364a18d454b5f1348006505ccd20
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-06-30 13:03:46 +0530

    Create Is Binary Number Multiple of 3.cpp

---
+++ GeeksForGeeks/Is Binary Number Multiple of 3.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isDivisible(string s){
+	    //complete the function here
+	    int rem = 0;
+	    for(char c: s) {
+	        rem  = (rem * 2 + (c-'0')) % 3;
+	    }
+	    if(rem==0)
+	        return 1;
+	    return 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isDivisible(s) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
