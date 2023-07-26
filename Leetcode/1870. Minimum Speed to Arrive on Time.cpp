ee2661cbdb31f99020a5231e92774e109f744818
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-07-26 19:31:04 +0530

    Create 1870. Minimum Speed to Arrive on Time.cpp

---
+++ Leetcode/1870. Minimum Speed to Arrive on Time.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double timeRequired(vector<int>& dist, int speed) {
+        double time = 0.0;
+        for (int i = 0 ; i < dist.size(); i++) {
+            double t = (double) dist[i] / (double) speed;
+            time += (i == dist.size() - 1 ? t : ceil(t));
+        }
+        return time;
+    }
+    
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int left = 1;
+        int right = 1e7;
+        int minSpeed = -1;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (timeRequired(dist, mid) <= hour) {
+                minSpeed = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return minSpeed;
+    }
+};
