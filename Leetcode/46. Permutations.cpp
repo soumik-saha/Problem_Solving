3adab36ad92c842e0585c157f2d079837bc2a82a
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-08-02 08:55:35 +0530

    Create 46. Permutations.cpp

---
+++ Leetcode/46. Permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    void solve(int ind, vector<int>& nums, vector<vector<int>>& ans) {
+        if(ind >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=ind; i<nums.size(); i++) {
+            swap(nums[i], nums[ind]);
+            solve(ind+1, nums, ans);
+            swap(nums[i], nums[ind]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solve(0, nums, ans);
+        return ans;
+    }
+};
