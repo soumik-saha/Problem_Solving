d8388fef01ad9c4bb6198cbb70ac2bd95aca7cce
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-08-09 19:42:19 +0530

    Create Largest prime factor.cpp

---
+++ GeeksForGeeks/Largest prime factor.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public: 
+    long long int largestPrimeFactor(int n){
+        // code here
+        long long int i=2;
+        while(n!=1) {
+            if(n%i==0) {
+                n/=i;
+            }
+            else {
+                i++;
+            }
+        }
+        return i;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
