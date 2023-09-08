80aacd027ab9a4c25dbf4e247819782c17243fad
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-09-08 08:29:51 +0530

    Create 118. Pascal's Triangle.cpp

---
+++ Leetcode/118. Pascal's Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        ans.push_back({1});
+
+        for(int i=1; i<numRows; i++) {
+            vector<int> row;
+            row.push_back(1);
+            for(int j=1; j<i; j++) {
+                row.push_back(ans[i-1][j-1] + ans[i-1][j]);
+            }
+            row.push_back(1);
+            ans.push_back(row);
+        }
+
+        return ans;
+    }
+};
