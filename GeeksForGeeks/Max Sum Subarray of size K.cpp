32b92b6b813604bac2c10afa34654645936eb8ee
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-12-11 08:25:51 +0530

    Create Max Sum Subarray of size K.cpp

---
+++ GeeksForGeeks/Max Sum Subarray of size K.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        // code here 
+        long ans = 0;
+        for(int j=0; j<K; j++) {
+            ans += Arr[j];
+        }
+        
+        long sum = ans;
+        for(int i=K, j=0; i<Arr.size(); i++, j++) {
+            sum += Arr[i] - Arr[j];
+            ans = max(ans, sum);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
