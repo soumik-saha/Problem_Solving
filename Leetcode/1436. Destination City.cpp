ccce123ed40bc5de4e8de9c76d1b0c996bdadfb5
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-12-15 21:31:08 +0530

    Create 1436. Destination City.cpp

---
+++ Leetcode/1436. Destination City.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string, bool> mp;
+        for(int i=0; i<paths.size(); i++) {
+            mp[paths[i][0]] = true;
+        }
+        for(int i=0; i<paths.size(); i++) {
+            if(mp.find(paths[i][1]) == mp.end()) {
+                return paths[i][1];
+            }
+        }
+        return "";
+    }
+};
