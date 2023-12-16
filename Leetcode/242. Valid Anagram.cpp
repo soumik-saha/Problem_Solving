e643b5539a6a2089a111fa7f6e77877eb310dd7c
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2023-12-16 22:57:15 +0530

    Create 242. Valid Anagram.cpp

---
+++ Leetcode/242. Valid Anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        unordered_map<char, int> freq;
+        for(int i=0; i<s.size(); i++) {
+            freq[s[i]]++;
+        }
+        for(int i=0; i<t.size(); i++) {
+            freq[t[i]]--;
+            if(freq[t[i]]<0) 
+                return false;
+        }
+        return true;
+    }
+};
