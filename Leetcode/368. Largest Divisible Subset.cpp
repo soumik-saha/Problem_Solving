9a4fcff8ce6c2317aa4c597498250655943f0d68
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-02-09 12:14:06 +0530

    Update 368. Largest Divisible Subset.cpp

--- Leetcode/368. Largest Divisible Subset.cpp
+++ Leetcode/368. Largest Divisible Subset.cpp
@@ -2,23 +2,32 @@ class Solution {
 public:
     unordered_map<int, vector<int>> dp;
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(begin(nums), end(nums));
+        int n = nums.size();
+        if(n==0)    return nums;
+        sort(nums.begin(), nums.end());
         vector<int> ans;
-        for(int i = 0; i < size(nums); i++) {
-            auto res = solve(nums, i);
-            if(size(res) > size(ans)) ans = res;
+        for(int i=0; i<n; i++) {
+            vector<int> subset = solve(i, nums);
+            if(ans.size()<subset.size()) {
+                ans = subset;
+            }
         }
         return ans;
     }
-    vector<int> solve(vector<int>& nums, int start) {
-        if(start >= size(nums)) return {};
-        if(dp.count(start)) return dp[start];
-        for(int next = start+1; next < size(nums); next++) {
-            if(nums[next] % nums[start]) continue;
-            auto res = solve(nums, next);
-            if(size(res) >= size(dp[start])) dp[start] = res;
+    vector<int> solve(int i, vector<int>& nums) {
+        if(dp[i].size()!=0) {
+            return dp[i];
         }
-        dp[start].push_back(nums[start]);
-        return dp[start];
+        vector<int> to_return;
+        for(int k=0; k<i; k++) {
+            if(nums[i]%nums[k]==0) {
+                vector<int> s = solve(k, nums);
+                if(s.size()>to_return.size()) {
+                    to_return = s;
+                }
+            }
+        }
+        to_return.push_back(nums[i]);
+        return dp[i] = to_return;
     }
 };
