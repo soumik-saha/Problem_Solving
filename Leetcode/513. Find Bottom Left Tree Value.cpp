e146846d620155712c576abcbc5cdba5aa813389
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-02-28 10:12:35 +0530

    Create 513. Find Bottom Left Tree Value.cpp

---
+++ Leetcode/513. Find Bottom Left Tree Value.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        int ans;
+
+        while(!q.empty()) {
+            int size = q.size();
+            ans = q.front()->val;
+
+            while(size--) {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left) {
+                    q.push(node->left);
+                }    
+                if(node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
