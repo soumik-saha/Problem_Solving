45392610268ad118b12dcfd4112ccbfff37637a5
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-03-09 08:56:08 +0530

    Create Find the N-th character.cpp

---
+++ GeeksForGeeks/Find the N-th character.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    char nthCharacter(string s, int r, int n) {
+        //code here
+        while(r--) {
+            string ds = "";
+            for(int i=0; i<=n; i++) {
+                if(s[i]=='1') {
+                    ds += "10";
+                } else {
+                    ds += "01";
+                }
+            }
+            s = ds;
+        }
+        return s[n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int R, N;
+        string S;
+        cin >> S >> R >> N;
+        Solution ob;
+        cout << ob.nthCharacter(S, R, N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
