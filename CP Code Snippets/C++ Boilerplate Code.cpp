73d0026005ffe0de1145442f1e6884795e6ae570
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-04-12 13:08:50 +0530

    Update C++ Boilerplate Code.cpp

--- CP Code Snippets/C++ Boilerplate Code.cpp
+++ CP Code Snippets/C++ Boilerplate Code.cpp
@@ -1,14 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
-int main(){
-    int t=1;
-    cin>>t;
-    while (t--)
-    {
-        // Code Here
-        
-    }
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("inputf.in", "r", stdin);
+	freopen("outputf.in", "w", stdout);
+#endif
+
+	ll t = 1;
+	cin >> t;
+
+	while (t--) {
+		// Your code here
+		int n;
+		cin >> n;
+
+
+	}
+
+	return 0;
 }
