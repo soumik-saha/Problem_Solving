aac242f2f1b16ace34b48d09c0da9cbe1644876d
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-04-25 11:48:08 +0530

    Create Maximum sum of hour glass.cpp

---
+++ GeeksForGeeks/Maximum sum of hour glass.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMaxSum(int n, int m, vector<vector<int>> mat) {
+        // code here
+        if(n<3 || m<3) return -1;
+        
+        int maxSum = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                int sum = 0;
+                if(i+2<n && j+2<m) {
+                    sum += mat[i][j] + mat[i][j+1] + mat[i][j+2];
+                    sum += mat[i+1][j+1];
+                    sum += mat[i+2][j] + mat[i+2][j+1] + mat[i+2][j+2];
+                }
+                maxSum = max(sum, maxSum);
+            }
+        }
+        
+        return maxSum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, i, j;
+        cin >> N >> M;
+        vector<vector<int>> Mat(N, vector<int>(M));
+        for (i = 0; i < N; i++)
+            for (j = 0; j < M; j++) cin >> Mat[i][j];
+        Solution ob;
+        cout << ob.findMaxSum(N, M, Mat) << "\n";
+    }
+}
+// } Driver Code Ends
