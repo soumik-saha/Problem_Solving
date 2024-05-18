4026534f15d53e15a3857d99fa500964c34371e1
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-05-18 10:47:46 +0530

    Create Find the Highest number.cpp

---
+++ GeeksForGeeks/Find the Highest number.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        int highest = a[0];
+        
+        for(int i=1; i<a.size(); i++) {
+            highest = max(highest, a[i]);
+        }
+        
+        return highest;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
