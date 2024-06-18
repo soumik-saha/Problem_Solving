e8ba982ebea47ef0a5a5fdc0753587c8caf8c544
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-06-18 15:40:08 +0530

    Create Number of Rectangles in a Circle.cpp

---
+++ GeeksForGeeks/Number of Rectangles in a Circle.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int rectanglesInCircle(int r) {
+        int d=4*r*r;
+        int ans=0;
+        for(long long i=1;i*1LL*i<4*r*r;i++){
+            long long m=sqrt(d-i*i);
+            ans=ans+m;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        int ans = ob.rectanglesInCircle(n);
+        cout << ans << "\n";
+    }
+}
+// } Driver Code Ends
