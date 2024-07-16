04cc8164f7711ed737f140ac3bcb308cd0189d6d
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-07-16 12:47:45 +0530

    Create Remaining String.cpp

---
+++ GeeksForGeeks/Remaining String.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        // Your code goes here
+        int n = s.size();
+        int i=0;
+        while(count>0 && i<n) {
+            if(s[i++]==ch) {
+                count--;
+            }
+        }
+        return s.substr(i);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
