5606d4e25ba4e971b9186952493f32019ebeb5bb
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-07-28 07:41:31 +0530

    Create Remove Duplicates.cpp

---
+++ GeeksForGeeks/Remove Duplicates.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+        vector<int> alpha(26, 0);
+        string ans;
+        for(char c:str) {
+            if(alpha[c-'a']==0) {
+                alpha[c-'a']++;
+                ans.push_back(c);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
