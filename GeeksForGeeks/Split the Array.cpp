004491f57cf6c076bfe21055640d5d69a67099c4
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-10-21 22:11:49 +0530

    Create Split the Array.cpp

---
+++ GeeksForGeeks/Split the Array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        int n = arr.size();
+        
+        int ans = 0;
+        
+        for(int i=0; i<n; i++) {
+            ans ^= arr[i];
+        }
+        
+        return ans==0 ? (1<<(n-1))-1 : 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
