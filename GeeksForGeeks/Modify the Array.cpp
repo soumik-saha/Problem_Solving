92766b3edfb57fce80f538e6cb5ad54c26e39e8e
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-10-24 15:42:46 +0530

    Create Modify the Array.cpp

---
+++ GeeksForGeeks/Modify the Array.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> modifyAndRearrangeArray(vector<int> &arr) {
+        // Complete the function
+        int zero= 0;
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i] == 0) {
+                zero++;
+            }
+            else {
+                if(i+1<arr.size() && arr[i]==arr[i+1]) {
+                    arr[i] *= 2;
+                    arr[i+1] = 0;
+                }
+            }
+        }
+        int idx = 0;
+        for(int i=0; i<arr.size(); i++) {
+            while(i<arr.size() && arr[i]==0) {
+                i++;
+            }
+            if(i<arr.size()) arr[idx++] = arr[i];
+        }
+        idx = arr.size()-1;
+        while(zero--) {
+            arr[idx--] = 0;
+        }
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.modifyAndRearrangeArray(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
