b7d8b404fd7e0ec589f6cc62e01918e7ab81bb7f
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-10-27 11:39:14 +0530

    Create Triplet Family.cpp

---
+++ GeeksForGeeks/Triplet Family.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool findTriplet(vector<int>& arr) {
+        // Your code
+        int n = arr.size();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                for(int k=0; k<n; k++) {
+                    if(i!=j && j!=k && arr[i]+arr[j]==arr[k]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        bool res = obj.findTriplet(arr);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
