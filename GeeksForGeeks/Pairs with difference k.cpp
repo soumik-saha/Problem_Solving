7d9657fb5159bace658401496b410f2e5ff9747f
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-10-30 11:45:09 +0530

    Create Pairs with difference k.cpp

---
+++ GeeksForGeeks/Pairs with difference k.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        unordered_map<long long, long long> mp;
+        long long ans = 0;
+        for(long long ele:arr) {
+            if(mp.find(ele-k)!=mp.end())
+                ans += mp[ele-k];
+            if(mp.find(ele+k)!=mp.end())
+                ans += mp[ele+k];
+            
+            mp[ele]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
