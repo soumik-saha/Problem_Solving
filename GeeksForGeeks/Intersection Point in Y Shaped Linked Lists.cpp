9ecccc48d01581665a092f2e7915dcd945204a15
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-11-13 19:39:52 +0530

    Update Intersection Point in Y Shaped Linked Lists.cpp

--- GeeksForGeeks/Intersection Point in Y Shaped Linked Lists.cpp
+++ GeeksForGeeks/Intersection Point in Y Shaped Linked Lists.cpp
@@ -1,34 +1,43 @@
 //{ Driver Code Starts
-//Initial Template for C++
+#include <iostream>
 
 #include <bits/stdc++.h>
 using namespace std;
 
 /* Link list Node */
-struct Node
-{
+struct Node {
     int data;
-    struct Node *next;
-    Node(int x)
-    {
+    struct Node* next;
+
+    Node(int x) {
         data = x;
         next = NULL;
     }
 };
 
-Node* inputList(int size)
-{
-    if (size == 0) return NULL;
+vector<int> take() {
+    vector<int> arr;
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    int number;
+    while (ss >> number) {
+        arr.push_back(number);
+    }
+    return arr;
+}
 
-    int val;
-    cin >> val;
+Node* inputList(int size, vector<int> v) {
+    if (size == 0)
+        return NULL;
 
-    Node *head = new Node(val);
-    Node *tail = head;
+    int val = v[0];
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        cin >> val;
+    Node* head = new Node(val);
+    Node* tail = head;
+
+    for (int i = 0; i < size - 1; i++) {
+        val = v[i + 1];
         tail->next = new Node(val);
         tail = tail->next;
     }
@@ -48,76 +57,67 @@ struct Node {
   }
 }; */
 
-class Solution
-{
-public:
-    int intersectPoint(Node* head1, Node* head2)
-    {
-        Node* first = head1, *sec = head2;
-        
-        bool changed1 = false, changed2 = false;
-        
-        while(true) {
-            if(first == sec)
-                return first->data;
-                
-            if(first->next)
-                first = first->next;
-            else if(!changed1) {
-                changed1 = true;
-                first = head2;
-            }
-            else
-                return -1;
-                
-            if(sec->next)
-                sec = sec->next;
-            else if(!changed2) {
-                changed2 = true;
-                sec = head1;
-            }
-            else
-                return -1;
+class Solution {
+  public:
+    // Function to find intersection point in Y shaped Linked Lists.
+    int intersectPoint(Node* head1, Node* head2) {
+        // Your Code Here
+        unordered_set<Node*> st;
+        while(head1) {
+            st.insert(head1);
+            head1 = head1->next;
         }
-        
+        while(head2) {
+            if(st.find(head2)!=st.end()) {
+                return head2->data;
+            }
+            head2 = head2->next;
+        }
         return -1;
     }
 };
 
 
-
 //{ Driver Code Starts.
 
-
 /* Driver program to test above function*/
-int main()
-{
+int main() {
+    srand(time(0));
     int T, n1, n2, n3;
 
     cin >> T;
-    while (T--)
-    {
-        cin >> n1 >> n2 >> n3;
+    cin.ignore();
+    while (T--) {
 
-        Node* head1 = inputList(n1);
-        Node* head2 = inputList(n2);
-        Node* common = inputList(n3);
+        vector<int> v1 = take();
+        vector<int> v2 = take();
+        vector<int> v3 = take();
+        int n1 = v1.size(), n2 = v2.size(), n3 = v3.size();
+
+        Node* head1 = NULL;
+        Node* head2 = NULL;
+        Node* common = NULL;
+
+        head1 = inputList(n1, v1);
+        head2 = inputList(n2, v2);
+        common = inputList(n3, v3);
 
         Node* temp = head1;
         while (temp != NULL && temp->next != NULL)
             temp = temp->next;
-        if (temp != NULL) temp->next = common;
+        if (temp != NULL)
+            temp->next = common;
 
         temp = head2;
         while (temp != NULL && temp->next != NULL)
             temp = temp->next;
-        if (temp != NULL) temp->next = common;
-        
+        if (temp != NULL)
+            temp->next = common;
         Solution ob;
         cout << ob.intersectPoint(head1, head2) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
-
 
 // } Driver Code Ends
