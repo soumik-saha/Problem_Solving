d8f0948092c796f23de564c53b5af4b8f7eff653
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-11-22 13:13:24 +0530

    Create Stock Buy and Sell – Max one Transaction Allowed.cpp

---
+++ GeeksForGeeks/Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int ans = 0;
+        int n = prices.size();
+        int minPrice = INT_MAX;
+        for(int i=0; i<n; i++) {
+            if(minPrice>prices[i]) {
+                minPrice = prices[i];
+            }
+            else {
+                ans = max(ans, prices[i]-minPrice);
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> prices;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            prices.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.maximumProfit(prices);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
