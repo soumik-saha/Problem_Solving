509755c28eab5c870fb56d7a29e903af40a7f91c
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-11-23 13:24:29 +0530

    Create Minimize the Heights I.java

---
+++ GeeksForGeeks/Minimize the Heights I.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Read the number of test cases (t)
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+            String input = br.readLine().trim();
+
+            // Split the input line into integers and store them in the array
+            String[] tokens = input.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            // Create an instance of the Solution class
+            Solution ob = new Solution();
+
+            // Call the getMinDiff method
+            int res = ob.getMinDiff(k, arr);
+
+            // Print the result
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int getMinDiff(int k, int[] arr) {
+        // code here
+        int n = arr.length;
+        Arrays.sort(arr);
+        int ans = arr[n-1]-arr[0];
+        for(int i=1; i<n; i++) {
+            int mini = Math.min(arr[0]+k, arr[i]-k);
+            int maxi = Math.max(arr[i-1]+k, arr[n-1]-k);
+            ans = Math.min(ans, maxi-mini);
+        }
+        return ans;
+    }
+}
