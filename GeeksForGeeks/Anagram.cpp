ddea392be4d805c76810f964e0e77d6b3fdabe9d
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-11-30 10:03:08 +0530

    Create Anagram.cpp

---
+++ GeeksForGeeks/Anagram.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // Your code here
+        unordered_map<char, int> mp1, mp2;
+        for(auto c:s1) {
+            mp1[c]++;
+        }
+        for(auto c:s2) {
+            mp2[c]++;
+        }
+        return mp1==mp2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
