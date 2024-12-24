e1914ae6ef7e3c6a93fb7b80e28b8bca9e673e6a
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-12-24 20:43:59 +0530

    Create Search in a sorted Matrix.cpp

---
+++ Leetcode/Search in a sorted Matrix.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0; i<n; i++) {
+            int idx = upper_bound(mat[i].begin(), mat[i].end(), x) - mat[i].begin();
+            if(idx>0 && mat[i][idx-1]==x)   return true;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.searchMatrix(matrix, x))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
