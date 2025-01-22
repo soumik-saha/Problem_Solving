8b831a253d3f0dac9ef533723a5537a6117d525f
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2025-01-22 11:10:47 +0530

    Create 1765. Map of Highest Peak.cpp

---
+++ Leetcode/1765. Map of Highest Peak.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> height(m, vector<int>(n, -1));
+
+        queue<pair<int, int>> q;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(isWater[i][j]) {
+                    height[i][j] = 0;
+                    q.push({i,j});
+                }
+            }
+        }
+
+        vector<pair<int, int>> dirs = {{-1,0}, {0,-1}, {0,1}, {1,0}};
+
+        while(!q.empty()) {
+            auto [x,y] = q.front();
+            q.pop();
+
+            for(auto dir:dirs) {
+                int nx = x+dir.first, ny = y+dir.second;
+                
+                if(nx>=0 && nx<m && ny>=0 && ny<n && height[nx][ny]==-1) {
+                    height[nx][ny] = height[x][y]+1;
+                    q.push({nx,ny});
+                }
+            }
+        }
+
+        return height;
+    }
+    // 0 0 1
+    // 1 0 0
+    // 0 0 0 
+
+    // 1 1 0
+    // 0 1 1
+    // 1 2 2
+
+// [0,0,0,0,0,0,1,0]
+// [0,1,0,0,0,0,0,0]
+// [0,0,0,0,0,0,0,0]
+// [0,0,0,0,0,0,0,0]
+// [0,0,0,0,0,0,0,0]
+// [0,0,0,0,0,0,0,0]
+// [0,0,0,0,0,0,1,0]
+// [0,0,1,0,0,0,0,0]
+};
