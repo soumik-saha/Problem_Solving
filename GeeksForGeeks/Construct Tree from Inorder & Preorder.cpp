82223d21f80098b2d30f99dbb5da5e2ea2c9f621
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2025-02-06 10:35:09 +0530

    Create Construct Tree from Inorder & Preorder.cpp

---
+++ GeeksForGeeks/Construct Tree from Inorder & Preorder.cpp
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition of the Node class
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Function to print the tree in postorder traversal
+void printPostOrder(Node *root) {
+    if (root == NULL)
+        return;
+    printPostOrder(root->left);
+    printPostOrder(root->right);
+    cout << root->data << " ";
+}
+
+
+// } Driver Code Ends
+// Class that contains the logic to build the binary tree
+/*
+Definition of the Node class
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to build the tree from given inorder and preorder traversals
+    Node *buildTree(vector<int> &inorder, vector<int> &preorder) {
+        // code here
+        int preStart = 0, inStart = 0, inEnd = inorder.size()-1;
+        return helper(inorder, preorder, inStart, preStart, inEnd);
+    }
+    Node* helper(vector<int>& inorder, vector<int>& preorder, int inStart, int& preStart, int inEnd) {
+        if(inStart>inEnd || preStart>=preorder.size())  return NULL;
+        
+        int rootVal = preorder[preStart];
+        Node* root = new Node(rootVal);
+        
+        int inIndex = inStart;
+        while(inorder[inIndex]!=rootVal) {
+            inIndex++;
+        }
+        
+        preStart++;
+        
+        root->left = helper(inorder, preorder, inStart, preStart, inIndex-1);
+        root->right = helper(inorder, preorder, inIndex+1, preStart, inEnd);
+        
+        return root;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Main function where the test cases are handled
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> inorder, preorder;
+
+        // Input the inorder traversal
+        string input;
+        getline(cin, input);
+        stringstream ss1(input);
+        int num;
+        while (ss1 >> num)
+            inorder.push_back(num);
+
+        // Input the preorder traversal
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> num)
+            preorder.push_back(num);
+
+        // Create Solution object and build the tree
+        Solution obj;
+        Node *root = obj.buildTree(inorder, preorder);
+
+        // Print the postorder traversal of the constructed tree
+        printPostOrder(root);
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
