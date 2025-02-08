713ca7305f65180719594c39f5114e901bf9bd9c
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2025-02-08 21:11:44 +0530

    Create Tree Boundary Traversal.cpp

---
+++ GeeksForGeeks/Tree Boundary Traversal.cpp
@@ -0,0 +1,199 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    return new Node(val);
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after splitting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+// Tree Nodea
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> boundaryTraversal(Node *root) {
+        // code here
+        if(!root)   return {};
+        vector<int> ans;
+        vector<int> leftPart, rightPart, bottomPart;
+        
+        ans.push_back(root->data);
+        dfsLeft(root->left, leftPart);
+        dfsRight(root->right, rightPart);
+        root->data = INT_MAX;
+        dfsBottom(root, bottomPart);
+        
+        reverse(rightPart.begin(), rightPart.end());
+        
+        for(int i=0; i<leftPart.size(); i++)   ans.push_back(leftPart[i]);
+        
+        for(int i=0; i<bottomPart.size(); i++)   ans.push_back(bottomPart[i]);
+        
+        for(int i=0; i<rightPart.size(); i++)   ans.push_back(rightPart[i]);
+        
+        return ans;
+    }
+    void dfsLeft(Node* root, vector<int>& leftPart) {
+        if(!root) {
+            return;
+        }
+        
+        if(root->left || root->right)
+            leftPart.push_back(root->data);
+        
+        if(root->left) {
+            dfsLeft(root->left, leftPart);
+        }
+        else {
+            dfsLeft(root->right, leftPart);
+        }
+    }
+    void dfsRight(Node* root, vector<int>& rightPart) {
+        if(!root) {
+            return;
+        }
+        
+        if(root->left || root->right)
+            rightPart.push_back(root->data);
+        
+        if(root->right) {
+            dfsRight(root->right, rightPart);
+        }
+        else{
+            dfsRight(root->left, rightPart);
+        }
+    }
+    void dfsBottom(Node* root, vector<int>& bottomPart) {
+        if(!root) {
+            return;
+        }
+        
+        if(root->data!=INT_MAX && !root->left && !root->right)
+            bottomPart.push_back(root->data);
+        
+        dfsBottom(root->left, bottomPart);
+        dfsBottom(root->right, bottomPart);
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function */
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector<int> res = ob.boundaryTraversal(root);
+        for (int i : res)
+            cout << i << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
