3b025d51504bed2622501cb8577fb90b9e54ee9d
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2025-03-11 08:32:46 +0530

    Create Ways to Reach the n'th Stair.cpp

---
+++ GeeksForGeeks/Ways to Reach the n'th Stair.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countWays(int n) {
+        // your code here
+        int prev = 1, curr = 1;
+        for(int i=2; i<=n; i++) {
+            int temp = prev + curr;
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    // taking total testcases
+    int t;
+    cin >> t;
+    while (t--) {
+        // taking stair count
+        int m;
+        cin >> m;
+        Solution ob;
+        cout << ob.countWays(m) << endl; // Print the output from our pre computed array
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
